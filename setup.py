59d6872ab2be111a743647a6bcad40ef3420cd50
Barnaby Gray <barnaby@pickle.me.uk>
2012-02-26 22:08:08 +0000

    Use branch in download_url

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
+__version__ = '0.1'
 
 setup(name='s3grep',
-      version='0.1',
+      version=__version__,
       description='grep for Amazon S3',
       author='Barnaby Gray',
       author_email='barnaby@pickle.me.uk',
       url='http://github.com/barnybug/s3grep/',
-      download_url='http://github.com/barnybug/s3grep/tarball/master#egg=s3grep-0.1',
+      download_url='http://github.com/barnybug/s3grep/tarball/'+__version__+'#egg=s3grep-'+__version__,
       requires=['boto'],
       scripts=['s3grep'],
       )
